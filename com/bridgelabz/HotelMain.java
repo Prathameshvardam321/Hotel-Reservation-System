f1b299a5d60b42f7ca9c12313d8175fbd6883e30
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-11-15 11:01:53 +0530

    Added HotelReservationUC8

--- com/bridgelabz/HotelMain.java
+++ com/bridgelabz/HotelMain.java
@@ -4,11 +4,12 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class HotelMain {
-    ArrayList<Hotel> hotelArrayList = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Week week = new Week();
-        ArrayList<Hotel> hotels = new ArrayList<>();
+        ArrayList<Hotel> hotelsForNormalCustomer = new ArrayList<>();
+        ArrayList<Hotel> hotelsForRegularCustomer = new ArrayList<>();
         System.out.println("Enter entry date : ");
         String entryDate = scanner.next();
         System.out.println("Entry exit date : ");
@@ -17,31 +18,49 @@ public class HotelMain {
         int weekend = (int) week.getWeekends(entryDate,exitDate);
         int userChoice ;
         do {
-            System.out.println("1.Add Details\t2.Add rating\t3.view");
+            System.out.println("1.Add Hotels\n2.view Hotels \n3.Best rated cheapest hotel");
             System.out.println("Enter choice : ");
             userChoice = scanner.nextInt();
             switch (userChoice){
                 case 1:Hotel hotel =new Hotel();
-                    System.out.println("choose customer\n 1.Normal customer\n2.Reward customer ");
+                    System.out.println("choose customer\n 1.Regular customer\n2.Reward customer  ");
                     int customerType = scanner.nextInt();
                     if (customerType==1){
                         hotel.inputToHotelToCustomer();
+                        hotel.setTotalExpense(hotel.expenseOfHotel(weekDays,weekend));
+                        hotelsForNormalCustomer.add(hotel);
                     }else if(customerType==2) {
                         hotel.inputToRewardCustomers();
+                        hotel.setTotalExpense(hotel.expenseOfHotel(weekDays,weekend));
+                        hotelsForRegularCustomer.add(hotel);
                     }else {
                         System.out.println("Enter valid input.");
                     }
                        break;
 
-                case 2:
-                    Iterator<Hotel> iterator = hotels.iterator();
+                case 2:System.out.println("choose customer\n 1.Regular customer\n2.Reward customer  ");
+                    customerType = scanner.nextInt();
+                    if (customerType==1){
+                        Iterator<Hotel> iterator =hotelsForNormalCustomer .iterator();
                     while (iterator.hasNext()){
                         hotel = iterator.next();
                         System.out.println(hotel);
                     }
+                    }else if (customerType==2) {
+                        Iterator<Hotel> iterator = hotelsForRegularCustomer.iterator();
+                        while (iterator.hasNext()) {
+                            hotel = iterator.next();
+                            System.out.println(hotel);
+                        }
+                    }
                     break;
+                case 3:
+                    Hotel firstHotel = hotelsForRegularCustomer.get(0);
+                    Hotel hotelCheapest = hotelsForRegularCustomer.stream().reduce(firstHotel,(a,b)->a.expenseOfHotel(weekDays,weekend)<b.expenseOfHotel(weekDays,weekend)&&a.getRating()>=b.getRating()?a:b);
+                    System.out.println(hotelCheapest);
                 default:
                     System.out.println("Enter valid inputs .");
+
             }
         }while (userChoice!=0);
     }
--- com/bridgelabz/Week.java
+++ com/bridgelabz/Week.java
@@ -10,54 +10,67 @@ import java.util.Map;
 public class Week {
 
     long getWeekends(String strStartDate, String strEndDate) {
-        Map<String, List<LocalDate>> map = new HashMap<>();
-        List<LocalDate> weekdays = new ArrayList<>();
-        List<LocalDate> weekends = new ArrayList<>();
-        map.put("weekdays", weekdays);
-        map.put("weekends", weekends);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
-        LocalDate startDate = LocalDate.parse(strStartDate, formatter);
-        LocalDate enddDate = LocalDate.parse(strEndDate, formatter);
+            Map<String, List<LocalDate>> map = new HashMap<>();
+            List<LocalDate> weekdays = new ArrayList<>();
+            List<LocalDate> weekends = new ArrayList<>();
+            try {
+            map.put("weekdays", weekdays);
+            map.put("weekends", weekends);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+            LocalDate startDate = LocalDate.parse(strStartDate, formatter);
+            LocalDate enddDate = LocalDate.parse(strEndDate, formatter);
 
-        for (LocalDate date = startDate; !date.isAfter(enddDate); date = date.plusDays(1)) {
-            switch (date.getDayOfWeek()) {
-                case MONDAY:
-                case TUESDAY:
-                case WEDNESDAY:
-                case THURSDAY:
-                case FRIDAY:
-                    weekdays.add(date);
-                    break;
-                case SATURDAY:
-                case SUNDAY:
-                    weekends.add(date);
+            for (LocalDate date = startDate; !date.isAfter(enddDate); date = date.plusDays(1)) {
+                switch (date.getDayOfWeek()) {
+                    case MONDAY:
+                    case TUESDAY:
+                    case WEDNESDAY:
+                    case THURSDAY:
+                    case FRIDAY:
+                        weekdays.add(date);
+                        break;
+                    case SATURDAY:
+                    case SUNDAY:
+                        weekends.add(date);
+                }
             }
+
+
+        }catch (Exception e){
+                System.out.println("Enter valid check in date.!!! ");
+        return weekdays.stream().count();
         }
         return weekends.stream().count();
     }
      long getWeekdays(String strStartDate, String strEndDate) {
-        Map<String, List<LocalDate>> map = new HashMap<>();
-        List<LocalDate> weekdays = new ArrayList<>();
-        List<LocalDate> weekends = new ArrayList<>();
-        map.put("weekdays", weekdays);
-        map.put("weekends", weekends);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
-        LocalDate startDate = LocalDate.parse(strStartDate, formatter);
-        LocalDate enddDate = LocalDate.parse(strEndDate, formatter);
-        for (LocalDate date = startDate; !date.isAfter(enddDate); date = date.plusDays(1)) {
-            switch (date.getDayOfWeek()) {
-                case MONDAY:
-                case TUESDAY:
-                case WEDNESDAY:
-                case THURSDAY:
-                case FRIDAY:
-                    weekdays.add(date);
-                    break;
-                case SATURDAY:
-                case SUNDAY:
-                    weekends.add(date);
-            }
-        }
-        return weekdays.stream().count();
-    }
+         Map<String, List<LocalDate>> map = new HashMap<>();
+         List<LocalDate> weekdays = new ArrayList<>();
+         List<LocalDate> weekends = new ArrayList<>();
+         try {
+             map.put("weekdays", weekdays);
+             map.put("weekends", weekends);
+             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+             LocalDate startDate = LocalDate.parse(strStartDate, formatter);
+             LocalDate enddDate = LocalDate.parse(strEndDate, formatter);
+             for (LocalDate date = startDate; !date.isAfter(enddDate); date = date.plusDays(1)) {
+                 switch (date.getDayOfWeek()) {
+                     case MONDAY:
+                     case TUESDAY:
+                     case WEDNESDAY:
+                     case THURSDAY:
+                     case FRIDAY:
+                         weekdays.add(date);
+                         break;
+                     case SATURDAY:
+                     case SUNDAY:
+                         weekends.add(date);
+                 }
+             }
+
+         }catch (Exception e){
+             System.out.println("Please enter valid checkout date !!!.");
+         return weekdays.stream().count();
+         }
+         return weekdays.stream().count();
+     }
 }
